1f52fc921f14c02f18accd2e2587da20fca0a98a
Younho Choo <yo+github@younho9.com>
2021-11-26 21:08:46 +0900

    test: remove pre create page step

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,5 @@
 import process from 'node:process';
 import dotenv from 'dotenv';
-import {NotionFaker} from '@narkdown/notion-faker';
 import {
   ListBlockChildrenResponse,
   ListDatabasesResponse,
@@ -8,68 +7,26 @@ import {
   QueryDatabaseResponse,
   SearchResponse,
 } from '@notionhq/client/build/src/api-endpoints';
+import {extractIdFromUrl} from '@narkdown/notion-utils';
 import {NarkdownClient} from '../src';
 
 dotenv.config();
 
 const NOTION_API_KEY = process.env.NOTION_API_KEY!;
-const ENTRY_POINT_PAGE_ID = process.env.ENTRY_POINT_PAGE_ID!;
 const narkdown: NarkdownClient = new NarkdownClient({auth: NOTION_API_KEY});
-const notionFaker: NotionFaker = new NotionFaker();
-const TIMEOUT = 1000 * 60 * 10;
+const TIMEOUT = 1000 * 60 * 2;
 
 describe('unlimited.blocks.children.list', () => {
-  const CHILD_COUNT = 101;
-  let TEST_PAGE_ID: string;
+  const TEST_PAGE_ID = extractIdFromUrl(
+    'https://narkdown.notion.site/et-tempore-iste-36d1060897c442399eb86e1f98dd31d4',
+  );
   let response: ListBlockChildrenResponse;
 
   beforeAll(async () => {
-    const {id: testPageId} = await narkdown.pages.create({
-      parent: {
-        page_id: ENTRY_POINT_PAGE_ID,
-      },
-      properties: {
-        title: notionFaker.page.properties.title()()(),
-      },
-    });
-
-    TEST_PAGE_ID = testPageId;
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    for (const _ of Array.from({length: CHILD_COUNT})) {
-      // eslint-disable-next-line no-await-in-loop
-      await narkdown.blocks.children.append({
-        block_id: TEST_PAGE_ID,
-        children: [
-          {
-            object: 'block',
-            type: 'heading_2',
-            heading_2: {
-              text: [
-                {
-                  type: 'text',
-                  text: {
-                    content: 'Lacinato kale',
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      });
-    }
-
     response = await narkdown.unlimited.blocks.children.list({
       block_id: TEST_PAGE_ID,
     });
   }, TIMEOUT);
-
-  afterAll(async () => {
-    await narkdown.blocks.delete({
-      block_id: TEST_PAGE_ID,
-    });
-  }, TIMEOUT);
-
   it(
     'has properties',
     async () => {
@@ -92,7 +49,7 @@ describe('unlimited.blocks.children.list', () => {
   it(
     'results.length is ROW_COUNT',
     async () => {
-      expect(response.results.length).toBe(CHILD_COUNT);
+      expect(response.results.length).toBeGreaterThan(100);
     },
     TIMEOUT,
   );
@@ -115,57 +72,14 @@ describe('unlimited.blocks.children.list', () => {
 });
 
 describe('unlimited.databases.query', () => {
-  const ROW_COUNT = 101;
-  let TEST_PAGE_ID: string;
-  let TEST_DATABASE_ID: string;
+  const TEST_DATABASE_ID = extractIdFromUrl(
+    'https://narkdown.notion.site/6b218cf027f54743ac7635fcbae61d18?v=c7e32351677e4d6b9936235d72a4def4',
+  );
   let response: QueryDatabaseResponse;
 
   beforeAll(async () => {
-    const {id: testPageId} = await narkdown.pages.create({
-      parent: {
-        page_id: ENTRY_POINT_PAGE_ID,
-      },
-      properties: {
-        title: notionFaker.page.properties.title()()(),
-      },
-      icon: notionFaker.icon.emoji(),
-    });
-
-    TEST_PAGE_ID = testPageId;
-
-    const {id: testDatabaseId} = await narkdown.databases.create({
-      parent: {
-        page_id: TEST_PAGE_ID,
-      },
-      title: notionFaker.database.title()()(),
-      properties: {
-        title: notionFaker.database.properties.title(),
-      },
-    });
-
-    TEST_DATABASE_ID = testDatabaseId;
-
-    await Promise.all(
-      Array.from({
-        length: ROW_COUNT,
-      }).map(async () =>
-        narkdown.pages.create({
-          parent: {database_id: TEST_DATABASE_ID},
-          properties: {
-            title: notionFaker.page.properties.title('address.cityName')()(),
-          },
-        }),
-      ),
-    );
-
     response = await narkdown.unlimited.databases.query({
       database_id: TEST_DATABASE_ID,
-    });
-  }, TIMEOUT);
-
-  afterAll(async () => {
-    await narkdown.blocks.delete({
-      block_id: TEST_PAGE_ID,
     });
   }, TIMEOUT);
 
@@ -191,7 +105,7 @@ describe('unlimited.databases.query', () => {
   it(
     'results.length is ROW_COUNT',
     async () => {
-      expect(response.results.length).toBe(ROW_COUNT);
+      expect(response.results.length).toBeGreaterThan(100);
     },
     TIMEOUT,
   );
@@ -316,66 +230,12 @@ describe('unlimited.users.list', () => {
 });
 
 describe('unlimited.search', () => {
-  const ROW_COUNT = 101;
   const SOME_UNIQUE_STRING_FOR_SEARCH = 'SOME_UNIQUE_STRING_FOR_SEARCH';
-  let TEST_PAGE_ID: string;
-  let TEST_DATABASE_ID: string;
   let response: SearchResponse;
 
   beforeAll(async () => {
-    const {id: testPageId} = await narkdown.pages.create({
-      parent: {
-        page_id: ENTRY_POINT_PAGE_ID,
-      },
-      properties: {
-        title: notionFaker.page.properties.title()()(),
-      },
-      icon: notionFaker.icon.emoji(),
-    });
-
-    TEST_PAGE_ID = testPageId;
-
-    const {id: testDatabaseId} = await narkdown.databases.create({
-      parent: {
-        page_id: TEST_PAGE_ID,
-      },
-      title: notionFaker.database.title()()(),
-      properties: {
-        title: notionFaker.database.properties.title(),
-      },
-    });
-
-    TEST_DATABASE_ID = testDatabaseId;
-
-    await Promise.all(
-      Array.from({
-        length: ROW_COUNT,
-      }).map(async () =>
-        narkdown.pages.create({
-          parent: {database_id: TEST_DATABASE_ID},
-          properties: {
-            title: {
-              title: [
-                {
-                  text: {
-                    content: SOME_UNIQUE_STRING_FOR_SEARCH,
-                  },
-                },
-              ],
-            },
-          },
-        }),
-      ),
-    );
-
     response = await narkdown.unlimited.search({
       query: SOME_UNIQUE_STRING_FOR_SEARCH,
-    });
-  }, TIMEOUT);
-
-  afterAll(async () => {
-    await narkdown.blocks.delete({
-      block_id: TEST_PAGE_ID,
     });
   }, TIMEOUT);
 
@@ -398,15 +258,13 @@ describe('unlimited.search', () => {
     TIMEOUT,
   );
 
-  // NOTE: The current search result is not accurate.
-  //
-  // it(
-  //   'results.length',
-  //   async () => {
-  //     expect(response.results.length).toBeGreaterThan(100);
-  //   },
-  //   TIMEOUT,
-  // );
+  it(
+    'results.length',
+    async () => {
+      expect(response.results.length).toBeGreaterThan(100);
+    },
+    TIMEOUT,
+  );
 
   it(
     'next_cursor is null',
